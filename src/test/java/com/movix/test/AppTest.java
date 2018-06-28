0f0db73aa34543b30f074317ab029b52538bbbdd
Paulo I. Carrasco González <paulo.carrasco.gonzalez@gmail.com>
2018-06-28 10:00:02 -0400

    Update AppTest.java

--- src/test/java/com/movix/test/AppTest.java
+++ src/test/java/com/movix/test/AppTest.java
@@ -15,7 +15,7 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( false );
+        assertTrue( true );
     }
     @Test
     public void testEnVivo()
